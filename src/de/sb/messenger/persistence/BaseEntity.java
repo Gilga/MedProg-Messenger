de6d80cb7c0b8a6630f04d362f5cae6541c3b492
dynex11 <ralen4e@yahoo.com>
2017-05-15 14:40:28 +0200

    Removed Test classes
    Tests are in separate package in JUnitTests branch

--- src/de/sb/messenger/persistence/BaseEntity.java
+++ src/de/sb/messenger/persistence/BaseEntity.java
@@ -1,16 +1,17 @@
 package de.sb.messenger.persistence;
 import java.util.Collections;
 import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
