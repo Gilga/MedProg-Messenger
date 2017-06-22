169150d7aff02d7009db2a36826f03998f6a321b
dynex11 <ralen4e@yahoo.com>
2017-06-22 20:27:53 +0200

    testLifecycle() added
    PUT and GET person. Changes to testIdentityQueries() - to use
    Baumeister ServiceTest class

--- test/de/sb/messenger/test/rest/PersonServiceTest.java
+++ test/de/sb/messenger/test/rest/PersonServiceTest.java
@@ -8,78 +8,156 @@ import static javax.ws.rs.core.MediaType.APPLICATION_XML_TYPE;
 import static javax.ws.rs.core.MediaType.MEDIA_TYPE_WILDCARD;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import java.net.URI;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import de.sb.messenger.persistence.Document;
 import de.sb.messenger.persistence.Person;
+import de.sb.messenger.persistence.Person.Group;
 
 public class PersonServiceTest extends ServiceTest {
-	Client client;
-	static private final URI SERVICE_URI = URI.create("http://localhost:8001/services");
-	String username;
-	String usernameAndPassword;
-	String authorizationHeaderName;
-	String authorizationHeaderValue;
-	String authorizationHeaderInvalidValue;
+	
+	
+	public String username;
+	public String usernameAndPassword;
+	public String authorizationHeaderName;
+	public String authorizationHeaderValue;
+	public String authorizationHeaderInvalidValue;
+
+	public EntityManager entityManager;
+	protected static EntityManagerFactory EM_FACTORY;
+	WebTarget userTargetValidInes;
+	WebTarget userTargetInvalid ;
+	WebTarget userTargetValid;
+	WebTarget userTargetInvalidUser;
 
 	@Before
 	public void setupBefore() {
-		client = ClientBuilder.newClient();
+		
 		username = "ines";
 		String password = "ines";
 		usernameAndPassword = username + ":" + password;
 		authorizationHeaderName = "Authorization";
 		authorizationHeaderValue = "Basic"
 				+ java.util.Base64.getEncoder().encodeToString(usernameAndPassword.getBytes());
-		client = ClientBuilder.newClient();
+		EM_FACTORY = Persistence.createEntityManagerFactory("messenger");
+		entityManager = EM_FACTORY.createEntityManager();
+
 	}
 
 	@Test
 	public void testCriteriaQueries() {
 		// target correct? ines plain text or encoded?
 
-
 	}
 
 	@Test
 	public void testIdentityQueries() {
-
-		Response response = client.target(SERVICE_URI + "/people").request()
+		 userTargetValidInes = this.newWebTarget("ines", "ines").path("people/1");
+		 userTargetInvalid = this.newWebTarget("ines", "password").path("people");
+		 userTargetValid = this.newWebTarget("ines", "ines").path("people");
+		 userTargetInvalidUser = this.newWebTarget("ines", "ines").path("/people/20");
+		
+		Response res = userTargetValid.request()
 				.header(authorizationHeaderName, authorizationHeaderValue).get();
 		// correct authentication.no exception
-		assertEquals(200, response.getStatus());
-		 
-		response = client.target(SERVICE_URI + "/people").request()
-				.header(authorizationHeaderName,authorizationHeaderInvalidValue).get();
-		//401 Unauthorized 
-		assertEquals(401, response.getStatus());
+		assertTrue(res.getStatus() == 200);
 
-		response = client.target(SERVICE_URI + "/people/" + 1).request()
-				.header(authorizationHeaderName, authorizationHeaderValue).get();
-		//valid. keine exception
-		assertEquals(200, response.getStatus());
+		res = userTargetInvalid.request().header(authorizationHeaderName, authorizationHeaderInvalidValue).get();
+		// 401 Unauthorized
+		assertTrue(res.getStatus() == 401);
 
-		response = client.target(SERVICE_URI + "/people/" + 20).request()
+		res = userTargetValidInes.request().header(authorizationHeaderName, authorizationHeaderValue).get();
+		// valid. keine exception
+		assertTrue(res.getStatus() == 200);
+
+		res = userTargetInvalidUser.request()
 				.header(authorizationHeaderName, authorizationHeaderValue).get();
-		//ClientErrorException 404keine passende Entitaet
-		assertEquals(404, response.getStatus());
+		// ClientErrorException 404keine passende Entitaet
+		assertTrue(res.getStatus() == 404);
+
+		res = userTargetValidInes.request().header(authorizationHeaderName, authorizationHeaderValue)
+				.accept(APPLICATION_JSON).get();
+		// return media type APPLICATION_JSON
+		assertEquals(APPLICATION_JSON_TYPE, res.getMediaType());
+
+		res = userTargetValidInes.request().header(authorizationHeaderName, authorizationHeaderValue)
+				.accept(APPLICATION_XML).get();
+		// returns media type APPLICATION_XML_TYPE because APPLICATION_XMLis
+		// simply a string
+		assertEquals(APPLICATION_XML_TYPE, res.getMediaType());
+		//this.getWasteBasket().add(userTargetIvalidUser.get)
+	}
+
+	@Test
+	public void ObserverRelationQueries() {
+		
+	}
+
+	@Test
+	public void testLifecycle() {
+		String s = "some content";
+		byte[] content = s.getBytes();
+		Document doc = new Document("image/jpeg", content);
+		// create entity
+		Person person = new Person("test@gmail.com", doc);
+		person.getName().setGiven("John");
+		person.getName().setFamily("Smith");
+		person.getAddress().setStreet("Falkenbergerstr. 1");
+		person.getAddress().setPostcode("12345");
+		person.getAddress().setCity("Berlin");
+		person.setGroup(Group.USER);
+		byte[] hash = person.passwordHash("password");
+		person.setPasswordHash(hash);
+
+		// add to the DB
+
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(doc);
+		transaction.commit();
+		transaction.begin();
+		entityManager.persist(person);
+		transaction.commit();
+		this.getWasteBasket().add(doc.getIdentiy());
+		this.getWasteBasket().add(person.getIdentiy());
+
+		transaction.begin();
+		person = entityManager.find(Person.class, person.getIdentiy());
 
 		
-		response = client.target(SERVICE_URI + "/people/" + 1).request()
-				.header(authorizationHeaderName, authorizationHeaderValue).accept(APPLICATION_JSON).get();
-		//return media type APPLICATION_JSON
-		assertEquals(APPLICATION_JSON_TYPE, response.getMediaType());
-
-		response = client.target(SERVICE_URI + "/people/" + 1).request()
-				.header(authorizationHeaderName, authorizationHeaderValue).accept(APPLICATION_XML).get();
-		//returns media type APPLICATION_XML_TYPE because APPLICATION_XMLis simply a string
-		assertEquals(APPLICATION_XML_TYPE, response.getMediaType());
+		// put a person
+		Response res =  userTargetValid.request()
+				.header(authorizationHeaderName, authorizationHeaderValue).header("Set-password", "Password")
+				.put(Entity.json(person));
+		// successful put
+		assertTrue(res.getStatus() == 200);
+		//
+		Long resIdentity = res.readEntity(Long.class);
+		// assertEquals(???,resIdentity)
+		
+		WebTarget userTargetPut = this.newWebTarget("john", "john").path("people/"+resIdentity);
+		
+		//get person 
+		Person personPut = userTargetPut.request()
+				.header(authorizationHeaderName, authorizationHeaderValue).get(Person.class);
+		assertEquals(person.getIdentiy(),personPut.getIdentiy());
+		
+		
+		
 	}
+	
+	//https://dennis-xlc.gitbooks.io/restful-java-with-jax-rs-2-0-2rd-edition/en/part1/chapter8/building_and_invoking_requests.html
 }
