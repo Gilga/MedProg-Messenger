79883ba211f7ce0df97421d44595513d94c886bd
dynex11 <ralen4e@yahoo.com>
2017-06-29 21:18:16 +0200

    Tests for creation, query param, create/update person

--- test/de/sb/messenger/rest/PersonServiceTest.java
+++ test/de/sb/messenger/rest/PersonServiceTest.java
@@ -2,23 +2,22 @@ package de.sb.messenger.rest;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
-import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
-import static javax.ws.rs.core.MediaType.TEXT_PLAIN_TYPE;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML_TYPE;
-import static javax.ws.rs.core.MediaType.MEDIA_TYPE_WILDCARD;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN_TYPE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-import java.net.URI;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
 import org.junit.Before;
@@ -27,11 +26,9 @@ import org.junit.Test;
 import de.sb.messenger.persistence.Document;
 import de.sb.messenger.persistence.Person;
 import de.sb.messenger.persistence.Person.Group;
-import de.sb.messenger.rest.EntityService;
 
 public class PersonServiceTest extends ServiceTest {
-	
-	
+
 	public String username;
 	public String usernameAndPassword;
 	public String authorizationHeaderName;
@@ -41,13 +38,13 @@ public class PersonServiceTest extends ServiceTest {
 	public EntityManager entityManager;
 	protected static EntityManagerFactory EM_FACTORY;
 	WebTarget userTargetValidInes;
-	WebTarget userTargetInvalid ;
+	WebTarget userTargetInvalid;
 	WebTarget userTargetValid;
 	WebTarget userTargetInvalidUser;
 
 	@Before
 	public void setupBefore() {
-		
+
 		username = "ines";
 		String password = "ines";
 		usernameAndPassword = username + ":" + password;
@@ -61,19 +58,82 @@ public class PersonServiceTest extends ServiceTest {
 
 	@Test
 	public void testCriteriaQueries() {
-		// target correct? ines plain text or encoded?
+		String s = "some content";
+		byte[] content = s.getBytes();
+		Document doc = new Document("image/jpeg", content);
+		// create entity
+		Person person = new Person("test@gmail.com", doc);
+		person.getName().setGiven("John");
+		person.getName().setFamily("Smith");
+		person.getAddress().setStreet("Falkenbergerstr. 1");
+		person.getAddress().setPostcode("12345");
+		person.getAddress().setCity("Berlin");
+		person.setGroup(Group.USER);
+		byte[] hash = person.passwordHash("password");
+		person.setPasswordHash(hash);
+		WebTarget webTarget = newWebTarget("root", "root").path("people");
+
+		/*
+		 * Test GET queryParam
+		 */
+		List<Person> people;
+
+		final int offset = 10, limit = 60;
+		Response response = webTarget.queryParam("offset", offset).queryParam("limit", limit).request()
+				.accept(APPLICATION_JSON).get();
+
+		people = response.readEntity(new GenericType<List<Person>>() {
+		});
+
+		for (Person p : people) {
+			assertTrue(offset <= p.getVersion());
+			assertTrue(limit >= p.getVersion());
+		}
+
+		assertTrue(response.getStatus() == 200);
+
+		/*
+		 * Test timeStamp
+		 */
+		final long lowerCreationTimestamp = 0, upperCreationTimestamp = 50;
+		response = webTarget.queryParam("lowerCreationTimestamp", lowerCreationTimestamp)
+				.queryParam("upperCreationTimestamp", upperCreationTimestamp).request().accept(APPLICATION_JSON).get();
+
+		people = response.readEntity(new GenericType<List<Person>>() {
+		});
+
+		for (Person p : people) {
+			assertTrue(lowerCreationTimestamp <= p.getCreationTimestamp());
+			assertTrue(upperCreationTimestamp >= p.getCreationTimestamp());
+		}
+		assertTrue(response.getStatus() == 200);
+		/*
+		 * Test givenName
+		 */
+		response = webTarget.queryParam("giveName", person.getName().getGiven()).request().accept(APPLICATION_JSON)
+				.get();
+
+		people = response.readEntity(new GenericType<List<Person>>() {
+		});
+		
+		assertEquals("John", people.get(0).getName().getGiven());
+		assertTrue(response.getStatus() == 200);
 
 	}
-
+/*
+ * Test authentification and 
+ */
 	@Test
 	public void testIdentityQueries() {
-		 userTargetValidInes = newWebTarget("ines", "ines").path("people/1");
-		 userTargetInvalid = newWebTarget("ines", "password").path("people");
-		 userTargetValid = newWebTarget("ines", "ines").path("people");
-		 userTargetInvalidUser = newWebTarget("ines", "ines").path("/people/20");
-		
-		Response res = userTargetValid.request()
-				.header(authorizationHeaderName, authorizationHeaderValue).get();
+		userTargetValidInes = newWebTarget("ines", "ines").path("people/1");
+		userTargetInvalid = newWebTarget("ines", "password").path("people");
+		userTargetValid = newWebTarget("ines", "ines").path("people");
+		userTargetInvalidUser = newWebTarget("ines", "ines").path("/people/20");
+
+		/*
+		 * Test getRequester
+		 */
+		Response res = userTargetValid.request().header(authorizationHeaderName, authorizationHeaderValue).get();
 		// correct authentication.no exception
 		assertTrue(res.getStatus() == 200);
 
@@ -85,13 +145,13 @@ public class PersonServiceTest extends ServiceTest {
 		// valid. keine exception
 		assertTrue(res.getStatus() == 200);
 
-		res = userTargetInvalidUser.request()
-				.header(authorizationHeaderName, authorizationHeaderValue).get();
+		res = userTargetInvalidUser.request().header(authorizationHeaderName, authorizationHeaderValue).get();
 		// ClientErrorException 404keine passende Entitaet
 		assertTrue(res.getStatus() == 404);
 
 		res = userTargetValidInes.request().header(authorizationHeaderName, authorizationHeaderValue)
 				.accept(APPLICATION_JSON).get();
+		
 		// return media type APPLICATION_JSON
 		assertEquals(APPLICATION_JSON_TYPE, res.getMediaType());
 
@@ -100,12 +160,12 @@ public class PersonServiceTest extends ServiceTest {
 		// returns media type APPLICATION_XML_TYPE because APPLICATION_XMLis
 		// simply a string
 		assertEquals(APPLICATION_XML_TYPE, res.getMediaType());
-		//.getWasteBasket().add(userTargetIvalidUser.ge)
+		// .getWasteBasket().add(userTargetIvalidUser.ge)
 	}
 
 	@Test
 	public void ObserverRelationQueries() {
-		
+
 	}
 
 	@Test
@@ -124,50 +184,37 @@ public class PersonServiceTest extends ServiceTest {
 		byte[] hash = person.passwordHash("password");
 		person.setPasswordHash(hash);
 
-		// add to the DB
+		WebTarget webTarget = newWebTarget("root", "root").path("people");
+		/*
+		 * test PUT update person/create person
+		 */
+		Response response = webTarget.request().accept(APPLICATION_JSON).header("Set-password", "password")
+				.put(Entity.json(person));
 
-//		EntityTransaction transaction = entityManager.getTransaction();
-//		transaction.begin();
-//		entityManager.persist(doc);
-//		transaction.commit();
-//		transaction.begin();
-//		entityManager.persist(person);
-//		transaction.commit();
-//		this.getWasteBasket().add(doc.getIdentiy());
-//		this.getWasteBasket().add(person.getIdentiy());
+		long idPerson = response.readEntity(Long.class);
+
+		assertNotEquals(0, idPerson);
+		assertTrue(response.getStatus() == 200);
+
+		this.getWasteBasket().add(idPerson);
+
+//		WebTarget userTargetPut = this.newWebTarget("john", "john").path("people/" + resIdentity);
 //
-//		transaction.begin();
-//		person = entityManager.find(Person.class, person.getIdentiy());
-
-		
-		// PUT a new person
-		Response res =  userTargetValid.request()
-				.header(authorizationHeaderName, authorizationHeaderValue).header("Set-password", "Password") //???
-				.put(Entity.json(person));
-		// successful put
-		assertTrue(res.getStatus() == 200);
-		//
-		long resIdentity = res.readEntity(Long.class).longValue();
-		assertEquals(0,resIdentity);
-		
-		WebTarget userTargetPut = this.newWebTarget("john", "john").path("people/"+resIdentity);
-		
-		//GET person 
-		Person personPut = userTargetPut.request()
-				.header(authorizationHeaderName, authorizationHeaderValue).get(Person.class);
-		assertEquals(person.getIdentiy(),personPut.getIdentiy());
-		
-		
-		person.getName().setFamily("Schroeder");
-		EntityService.update(entityManager, person);
-		//UPDATE person
-		 res =  userTargetValid.request()
-				.header(authorizationHeaderName, authorizationHeaderValue).header("Set-password", "Password").accept(TEXT_PLAIN)
-				.put(Entity.json(person));
-		
-		// assertEquals(,res.readEntity(Long.class));
-		 assertEquals(TEXT_PLAIN_TYPE, res.getMediaType());
+//		// GET person
+//		Person personPut = userTargetPut.request().header(authorizationHeaderName, authorizationHeaderValue)
+//				.get(Person.class);
+//		assertEquals(person.getIdentiy(), personPut.getIdentiy());
+//
+//		person.getName().setFamily("Schroeder");
+//		EntityService.update(entityManager, person);
+//		// UPDATE person
+//		res = userTargetValid.request().header(authorizationHeaderName, authorizationHeaderValue)
+//				.header("Set-password", "Password").accept(TEXT_PLAIN).put(Entity.json(person));
+//
+//		// assertEquals(,res.readEntity(Long.class));
+//		assertEquals(TEXT_PLAIN_TYPE, res.getMediaType());
 	}
-	
-	//https://dennis-xlc.gitbooks.io/restful-java-with-jax-rs-2-0-2rd-edition/en/part1/chapter8/building_and_invoking_requests.html
+
+	// https://dennis-xlc.gitbooks.io/restful-java-with-jax-rs-2-0-2rd-edition/en/part1/chapter8/building_and_invoking_requests.html
+	// https://stackoverflow.com/questions/27211012/how-to-send-json-object-from-rest-client-using-javax-ws-rs-client-webtarget
 }
