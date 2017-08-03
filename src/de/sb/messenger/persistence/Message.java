2542578d9b18b57207895397a18c2011cd86b4a9
Gilga <gilgatx@gmail.com>
2017-08-03 23:34:19 +0200

    message xml fix

--- src/de/sb/messenger/persistence/Message.java
+++ src/de/sb/messenger/persistence/Message.java
@@ -8,23 +8,33 @@ import javax.persistence.ManyToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 @Entity
 @Table(schema="messenger", name = "Message")
 @DiscriminatorValue(value = "Message")
 @PrimaryKeyJoinColumn(name="messageIdentity")
+@XmlRootElement
+@XmlType
 public class Message extends BaseEntity {
 	
 	@ManyToOne
 	@JoinColumn(name="authorReference", nullable = false)
+	@XmlElement
 	private Person author;
 	
 	@ManyToOne
 	@JoinColumn(name="subjectReference", nullable = false)
+	@XmlElement
 	private BaseEntity subject;
 	
 	@Column(name = "body", nullable = false)
 	@Size(min = 1, max = 4093)
+	@XmlElement
 	private String body;
 
 	public Message(Person author, BaseEntity subject, String body) {
